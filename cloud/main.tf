137b528ed238892df95f1ab7d6ca10427c91dd55
A.Polyakov <a.polyakov@unlimint.com>
2024-05-17 12:21:09 +0300

    clickhouse cloud

--- cloud/main.tf
+++ cloud/main.tf
@@ -9,3 +9,50 @@ terraform {
 
 provider "yandex" {
 }
+
+resource "yandex_vpc_network" "default" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.5.0.0/24"]
+  zone           = "ru-central1-a"
+}
+
+resource "yandex_mdb_clickhouse_cluster" "clickhouse-dev" {
+  environment             = "PRESTABLE"
+  name                    = "clickhouse-dev"
+  network_id              = yandex_vpc_network.default.id
+  version                 = "23.3"
+  admin_password          = var.clickhouse_password
+  sql_database_management = true
+  sql_user_management     = true
+
+  clickhouse {
+    resources {
+      resource_preset_id = "s3-c2-m8"
+      disk_type_id       = "network-ssd"
+      disk_size          = 64
+    }
+
+    config {
+      log_level              = "TRACE"
+      max_concurrent_queries = 100
+      max_connections        = 100
+    }
+  }
+
+  host {
+    type             = "CLICKHOUSE"
+    zone             = "ru-central1-a"
+    assign_public_ip = true
+    subnet_id        = yandex_vpc_subnet.foo.id
+  }
+
+  cloud_storage {
+    enabled = false
+  }
+
+  maintenance_window {
+    type = "ANYTIME"
+  }
+}
